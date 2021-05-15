e759c23ae1f38a53ec4c75bd4922e0b90243f3cb
Samuel Martin <s.martin49@gmail.com>
2021-05-15 18:24:44 +0200

    test_viewer.py: Make pylint happier

--- poc/test_viewer.py
+++ poc/test_viewer.py
@@ -1,17 +1,23 @@
+""" PyQt6 sample program: Image viewer.
+
+"""
+
 import sys
 import os
 
-from datetime import datetime, timezone
 from random import choices
 
-from PyQt6.QtWidgets import QWidget, QApplication, QLabel, QVBoxLayout, QMainWindow
-from PyQt6.QtGui import QFont, QImage, QImageReader, QPainter
+from PyQt6.QtWidgets import QWidget, QApplication, QVBoxLayout, QMainWindow
+from PyQt6.QtGui import QImageReader, QPainter
 from PyQt6.QtCore import Qt, QRect, QTimer, QPoint
 
 
 class ViewerWidget(QWidget):
+    """ Image viewer widget
+
+    """
     def __init__(self, get_image, bg_color=Qt.GlobalColor.black, geometry=None, parent=None):
-        super(ViewerWidget, self).__init__(geometry=geometry, parent=parent)
+        super().__init__(geometry=geometry, parent=parent)
 
         if not geometry:
             size = parent.size() if parent else self.size()
@@ -22,46 +28,67 @@ class ViewerWidget(QWidget):
 
         self.setGeometry(geometry)
 
-    def paintEvent(self, e=None):
-        self.setGeometry(self.parentWidget().frameGeometry())
-        qp = QPainter();
-        qp.begin(self)
-        self.draw(qp)
-        qp.end()
+    # pylint: disable=invalid-name
+    # pylint: disable=unused-argument
+    def paintEvent(self, event=None):
+        """ Paint event handler overload
 
-    def draw(self, qp):
-        qp.setPen(self.bg_color)
-        qp.setBrush(self.bg_color)
-        qp.drawRect(self.geometry())
+        """
+        self.setGeometry(self.parentWidget().frameGeometry())
+        qpainter = QPainter()
+        qpainter.begin(self)
+        self.draw(qpainter)
+        qpainter.end()
+
+    def draw(self, qpainter):
+        """ Draw the viewer widget
+
+        :param qpainter: QPainter instance
+        """
+        qpainter.setPen(self.bg_color)
+        qpainter.setBrush(self.bg_color)
+        qpainter.drawRect(self.geometry())
         image = self.get_image(self.size())
 
         pos = QPoint((self.width() - image.width()) // 2, (self.height() - image.height()) // 2)
-        qp.drawImage(QRect(pos, image.size()), image)
+        qpainter.drawImage(QRect(pos, image.size()), image)
 
 
 class Window(QWidget):
+    """ Main window widget
+
+    """
 
     IMAGE_FORMATS = ('bmp', 'gif', 'jpg', 'jpeg', 'png', 'pbm', 'pgm', 'ppm', 'xbm', 'xpm')
+    """ Supported image formats
+
+    """
 
     DELAY = 3000 # 3s
     COUNT = 10
 
     def __init__(self, image_dir, geometry=None, parent=None):
-        super(Window, self).__init__(geometry=geometry, parent=parent)
-        self.init_UI(image_dir, parent)
+        super().__init__(geometry=geometry, parent=parent)
+        self.image = None
+        self.image_path = None
+        self.init_ui(image_dir)
 
-    def init_UI(self, image_dir, parent=None):
+    def init_ui(self, image_dir):
+        """ Initialize the UI
 
+        :param image_dir: Path of the root image directory to be displayed
+
+        """
         images = []
-        for r, _, fs in os.walk(image_dir):
-            images.extend([os.path.join(r, f) for f in fs
-                           if f.rsplit('.', 1)[-1].lower() in self.IMAGE_FORMATS])
+        for root, _, files in os.walk(image_dir):
+            images.extend([os.path.join(root, fname) for fname in files
+                           if fname.rsplit('.', 1)[-1].lower() in self.IMAGE_FORMATS])
         self.images = choices(images, k=self.COUNT)
         self.image = None
         self.image_path = None
 
-        for p in self.images:
-            print(p)
+        for path in self.images:
+            print(path)
 
         geometry = self.frameGeometry()
         self.setGeometry(geometry)
@@ -80,6 +107,14 @@ class Window(QWidget):
         self.start()
 
     def get_image(self, size):
+        """ Return the image object to be displayed in the give size window
+
+        Original aspect of the image is kept, but image is scale to fill the most the window size.
+
+        :param size: The window size
+
+        :return: The QImage object
+        """
         path = self.images[self.step]
         if self.image_path != path:
             self.image_path = path
@@ -91,6 +126,9 @@ class Window(QWidget):
 
 
     def start(self):
+        """ Start the image timer
+
+        """
         self.step += 1
         if self.step >= len(self.images):
             sys.exit()
@@ -99,20 +137,20 @@ class Window(QWidget):
         self.repaint()
 
 
-def main():
+def __main__():
     app = QApplication([])
-    w = QMainWindow()
-    w.setWindowFlags(Qt.WindowFlags.WindowStaysOnTopHint |
-                     Qt.WindowFlags.CustomizeWindowHint |
-                     Qt.WindowFlags.MaximizeUsingFullscreenGeometryHint |
-                     Qt.WindowFlags.FramelessWindowHint)
-    geometry = QRect(QPoint(0, 0), w.screen().size())
-    w.setGeometry(geometry)
-    w.setCentralWidget(Window(sys.argv[1], geometry=geometry, parent=w))
-    w.showFullScreen()
-    w.setWindowTitle(os.path.basename(__file__))
+    window = QMainWindow()
+    window.setWindowFlags(Qt.WindowFlags.WindowStaysOnTopHint |
+                          Qt.WindowFlags.CustomizeWindowHint |
+                          Qt.WindowFlags.MaximizeUsingFullscreenGeometryHint |
+                          Qt.WindowFlags.FramelessWindowHint)
+    geometry = QRect(QPoint(0, 0), window.screen().size())
+    window.setGeometry(geometry)
+    window.setCentralWidget(Window(sys.argv[1], geometry=geometry, parent=window))
+    window.showFullScreen()
+    window.setWindowTitle(os.path.basename(__file__))
     app.exec()
 
 
 if __name__ == '__main__':
-    main()
+    __main__()
